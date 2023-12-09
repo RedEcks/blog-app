0f97c6dd7dedbd1ea641f0cebad5d38195d7bbf4
Xavier Dunkley <xavierxv18@gmail.com>
2023-12-08 19:10:08 -0500

    added logo and url naming

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from . import views 
-import re
+from . import views
 
-
+app_name = 'articles'
 
 urlpatterns = [
-    path("", views.article_list),
-    path("<slug:slug>",views.article_detail)
+    path("", views.article_list, name="list"),
+    path("<slug:slug>",views.article_detail, name="detail")
 ]
